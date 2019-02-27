768610b5ae156d0c14e1cffd35f3d1003c608890
roi <roi@ada.fis.ucm.es>
2019-02-27 18:36:34 +0100

    easy dlm

---
+++ src/train_test_dlm.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import feather
+import datetime
+from pydlm_lite import dlm, trend
+
+df = (feather.read_dataframe('/home/SHARED/SOLAR/data/oahu_min.feather')
+             .set_index('Datetime'))
+df[df < 0] = 0
+
+## Try DLM in one sensor
+
+GH_DH4 = df.GH_DH4
+train = GH_DH4[:"2011-07-31"]
+test = GH_DH4[:"2011-08-01"]
+##
+dlm_ghdh4 = dlm(train)
+dlm_ghdh4 = dlm_ghdh4 + trend(degree=2, discount=0.90, name='ltrend', w=10.0) ## Just a linear trend
+
+dlm_ghdh4.fitForwardFilter()
+
+preds = np.zeros(len(test))
+for i in range(len(test)):	
+    preds[i] = dlm_ghdh4.predictN(1)[0][0]
+    ##
+    dlm_ghdh4.append([test[i]])
+    dlm_ghdh4.fitForwardFilter()
+
+np.savetxt('predictions.out', (test,preds))   # x,y,z equal sized 1D arrays
+mae =np.mean( np.abs(test.values - preds) )
+print(mae)
