841769492343e86c27744863740dc2497e748489
atorres <alberto.torres@icmat.es>
2019-06-20 17:21:30 +0200

    update notebook

--- src/utils/models.py
+++ src/utils/models.py
@@ -1,7 +1,7 @@
 import keras
 from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Activation, Flatten, Input, Lambda, Reshape, Add, Multiply, Subtract, Dropout
-from keras.layers import Conv2D, MaxPooling2D, LocallyConnected1D, Conv1D, UpSampling1D, MaxPooling1D, Dot, Concatenate
+from keras.layers import Conv2D, MaxPooling2D, LocallyConnected1D, Conv1D, UpSampling1D, MaxPooling1D, Dot, Concatenate, LSTM, TimeDistributed
 
 def conv1D_lon(idx_sensor, n_sensors=16):
     ''' Returns a model using all the sensors to predict index_sensor '''
@@ -68,4 +68,27 @@ def conv1D_lon_lat(idx_sensor, n_sensors=16):
     xo_m = Activation('relu')(xo_m)
 
     model = Model(inputs=[xin, yin], outputs=[xo_m])
+    return model
+
+
+def conv1D_lon_LSTM(n_steps=3, n_sensors=16):
+    ''' Returns a model using all the sensors to predict index_sensor '''
+    xin = Input(shape=(n_steps, n_sensors, 1), name='main_input')
+    x = TimeDistributed(
+            LocallyConnected1D(8, 7,  data_format = 'channels_last', padding='valid', activation='relu')
+        )(xin)
+    x = TimeDistributed(
+            LocallyConnected1D(16, 5, data_format = 'channels_last', padding='valid', activation='relu')
+        )(x)
+    x = TimeDistributed(
+            Conv1D(32, 3, data_format = 'channels_last', padding='causal')
+        )(x)
+    xl = TimeDistributed(
+            Flatten()
+        )(x)
+    xl = LSTM(20)(xl)
+    xl = Dropout(0.2)(xl)
+    xo = Dense(n_sensors)(xl)
+
+    model = Model(inputs=[xin], outputs=[xo])
     return model
